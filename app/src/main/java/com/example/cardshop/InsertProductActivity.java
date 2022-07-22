706ea77a905c62e0b7131429a2c5c9419207cfa6
GiuseppeNapoli <napolig05@gmail.com>
2022-07-22 15:36:39 +0200

    Modify implementation Delete & insert update

--- app/src/main/java/com/example/cardshop/InsertProductActivity.java
+++ app/src/main/java/com/example/cardshop/InsertProductActivity.java
@@ -2,16 +2,28 @@ package com.example.cardshop;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -19,6 +31,9 @@ import java.util.Objects;
 public class InsertProductActivity extends AppCompatActivity {
 
     private static FirebaseFirestore firestore;
+    Uri filePath;
+    Button select;
+    final int PICK_IMAGE_REQUEST = 22;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,8 @@ public class InsertProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert_product);
         Objects.requireNonNull(getSupportActionBar()).hide(); //nasconde la barra superiore
         firestore = FirebaseFirestore.getInstance();
+        select = findViewById(R.id.buttonImage);
+        select.setOnClickListener(v -> SelectImage());
     }
 
     public void goBack(View view) {
@@ -33,8 +50,21 @@ public class InsertProductActivity extends AppCompatActivity {
         finish();
     }
 
-    public void addImage(View view) {
-        //TODO
+    public String addImage() {
+        //utilizzato per selezionare il nome dell'immagine correttamente
+        String path = filePath.getPath();
+        int index = path.lastIndexOf('/');
+        String name = path.substring(index);
+        path = "/immaginiCarte" + name;
+        return path;
+    }
+
+    private void SelectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleziona l'immagine"), PICK_IMAGE_REQUEST);
+        //uploadImage();
     }
 
     public void insertCard(View view) {
@@ -43,10 +73,55 @@ public class InsertProductActivity extends AppCompatActivity {
         docData.put("description", ((EditText) findViewById(R.id.editTextDescription)).getText().toString());
         docData.put("price", ((EditText) findViewById(R.id.editTextPrice)).getText().toString());
         docData.put("game", ((EditText) findViewById(R.id.editTextGame)).getText().toString());
+        docData.put("rating", Double.parseDouble(((EditText) findViewById(R.id.editTextRating)).getText().toString()));
+        docData.put("image", addImage());
 
         firestore.collection("cards")
                 .add(docData)
                 .addOnSuccessListener(aVoid -> Log.d(TAG, "DocumentSnapshot successfully written!"))
                 .addOnFailureListener(e -> Log.w(TAG, "Error writing document", e));
+    }
+
+    // Override metodo onActivityResult
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+
+        // si controlla il requestiCode e il resulCode
+        // se requestCode è uguale a PICK_IMAGE_REQUEST e resultCode è uguale a RESULT_OK
+        // si può settare l'immagine
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            // si ottiene il path
+            filePath = data.getData();
+            try {
+                // si setta l'immagine con il metodo della bitmap
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                //in caso di eccezioni si stampano
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void uploadImage(){
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(InsertProductActivity.this, "Successfully uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(InsertProductActivity.this, "Failure to upload", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/cardshop/adapters/AdminModifyCardAdapter.java
@@ -0,0 +1,79 @@
+package com.example.cardshop.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.cardshop.R;
+import com.example.cardshop.model.AdminModel;
+import com.example.cardshop.model.CardModel;
+import com.example.cardshop.model.CustomerModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+public class AdminModifyCardAdapter extends ArrayAdapter<CardModel> {
+    public AdminModifyCardAdapter(Context context, int textViewResourceId, List<CardModel> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    @SuppressLint({"ViewHolder", "InflateParams"})
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.card_admin_modify, null);
+        TextView name = convertView.findViewById(R.id.nameCardModify);
+        TextView description = convertView.findViewById(R.id.descriptionCardModify);
+        TextView game = convertView.findViewById(R.id.gameCardModify);
+        TextView price = convertView.findViewById(R.id.priceCardModify);
+        RatingBar rating = convertView.findViewById(R.id.ratingCardModify);
+        ImageButton modify = convertView.findViewById(R.id.modifyButton);
+        ImageView image = convertView.findViewById(R.id.imageCardModify);
+
+        CardModel card = getItem(position);
+        name.setText(card.getName());
+        description.setText(card.getDescription());
+        game.setText(card.getGame());
+        price.setText(card.getPrice());
+        rating.setRating((float) Double.parseDouble(String.valueOf(card.getRating())));
+
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+
+        StorageReference imageReference = storageReference.child(card.getImage());
+
+        long MAXBYTES = 1024 * 1024;
+
+        imageReference.getBytes(MAXBYTES).addOnSuccessListener(bytes -> {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            image.setImageBitmap(bitmap);
+        }).addOnFailureListener(e -> {});
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        /*delete.setOnClickListener(view -> {
+            System.out.println("Cancellazione prodotto: " + card.getName());
+            firestore.collection("cards").document(card.getName())
+                    .delete()
+                    .addOnSuccessListener(e -> System.out.println("Cancellazione effettuata"))
+                    .addOnFailureListener(e -> System.out.println("Cancellazione fallita"));
+        });*/
+
+        return convertView;
+    }
+}
